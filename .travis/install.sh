fe4ae858a0ae94022041c0a2d7c30d0edf42afa2
kfinley <kyle@kylefinley.net>
2020-12-21 12:55:45 -0800

    Fix: bash syntax. Travis needs tested. This is a best guess attempt for multi-os install setup.

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,14 +3,14 @@
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
     curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --runtime dotnet --version 3.1.10
     brew install --cask powershell
-else if [$TRAVIS_OS_NAME = 'linux']: then
+elif [$TRAVIS_OS_NAME = 'linux']; then
     sudo apt-get update
     sudo apt-get install -y wget apt-transport-https software-properties-common
     wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
     sudo apt-get update
     sudo apt-get install -y aspnetcore-runtime-3.1 powershell
-else # windows
+else
     # chocolatey v0.10.15 is installed on the Travis default windows image 
     choco install dotnetcore-runtime.install --version=3.1.10
     # TODO: Does the Travis default windows image have powershell?
