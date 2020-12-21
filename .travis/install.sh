1d6223d4c200f04debed2918a12609121e945756
kfinley <kyle@kylefinley.net>
2020-12-21 14:17:01 -0800

    Fix: Linux install section in .travis/install.sh

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,8 +6,11 @@ if [[ "${TRAVIS_OS_NAME}" = 'osx' ]]; then
 elif [[ "${TRAVIS_OS_NAME}" = 'linux' ]]; then
     sudo apt-get update
     sudo apt-get install -y wget apt-transport-https software-properties-common
+    wget http://mirrors.edge.kernel.org/ubuntu/pool/main/i/icu/libicu55_55.1-7_amd64.deb
+    wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl1.0/libssl1.0.0_1.0.2n-1ubuntu5.5_amd64.deb
     wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
-    sudo dpkg -i packages-microsoft-prod.deb
+    sudo dpkg -i libicu55_55.1-7_amd64.deb libssl1.0.0_1.0.2n-1ubuntu5.5_amd64.deb packages-microsoft-prod.deb
+    rm *.deb
     sudo apt-get update
     sudo apt-get install -y aspnetcore-runtime-3.1 powershell
 else
